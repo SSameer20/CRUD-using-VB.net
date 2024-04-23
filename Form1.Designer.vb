ac538269a5cc370aadd27c46d0d1d24d4ac1862e
Sameer Shaik <sameer.kattubadi@gmail.com>
2024-04-23 10:44:49 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblProduct = New Label()
+        lblModel = New Label()
+        lblUnitPrice = New Label()
+        lblBrand = New Label()
+        lblCategory = New Label()
+        txtProduct = New TextBox()
+        txtModel = New TextBox()
+        txtPrice = New TextBox()
+        ddlBrand = New ComboBox()
+        ddlCategory = New ComboBox()
+        productsGV = New DataGridView()
+        btnSave = New Button()
+        btnClear = New Button()
+        btnUpdate = New Button()
+        btnDelete = New Button()
+        btnShow = New Button()
+        CType(productsGV, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' lblProduct
+        ' 
+        lblProduct.AutoSize = True
+        lblProduct.Location = New Point(64, 53)
+        lblProduct.Name = "lblProduct"
+        lblProduct.Size = New Size(104, 20)
+        lblProduct.TabIndex = 0
+        lblProduct.Text = "Product Name"
+        ' 
+        ' lblModel
+        ' 
+        lblModel.AutoSize = True
+        lblModel.Location = New Point(64, 101)
+        lblModel.Name = "lblModel"
+        lblModel.Size = New Size(84, 20)
+        lblModel.TabIndex = 1
+        lblModel.Text = "Model Year"
+        ' 
+        ' lblUnitPrice
+        ' 
+        lblUnitPrice.AutoSize = True
+        lblUnitPrice.Location = New Point(66, 148)
+        lblUnitPrice.Name = "lblUnitPrice"
+        lblUnitPrice.Size = New Size(72, 20)
+        lblUnitPrice.TabIndex = 2
+        lblUnitPrice.Text = "Unit Price"
+        lblUnitPrice.TextAlign = ContentAlignment.TopRight
+        lblUnitPrice.UseMnemonic = False
+        ' 
+        ' lblBrand
+        ' 
+        lblBrand.AutoSize = True
+        lblBrand.Location = New Point(67, 198)
+        lblBrand.Name = "lblBrand"
+        lblBrand.Size = New Size(65, 20)
+        lblBrand.TabIndex = 3
+        lblBrand.Text = "Brand Id"
+        ' 
+        ' lblCategory
+        ' 
+        lblCategory.AutoSize = True
+        lblCategory.Location = New Point(70, 243)
+        lblCategory.Name = "lblCategory"
+        lblCategory.Size = New Size(91, 20)
+        lblCategory.TabIndex = 4
+        lblCategory.Text = "ddlCategory"
+        ' 
+        ' txtProduct
+        ' 
+        txtProduct.Location = New Point(176, 52)
+        txtProduct.Name = "txtProduct"
+        txtProduct.Size = New Size(125, 27)
+        txtProduct.TabIndex = 5
+        ' 
+        ' txtModel
+        ' 
+        txtModel.Location = New Point(174, 100)
+        txtModel.Name = "txtModel"
+        txtModel.Size = New Size(125, 27)
+        txtModel.TabIndex = 6
+        ' 
+        ' txtPrice
+        ' 
+        txtPrice.Location = New Point(176, 144)
+        txtPrice.Name = "txtPrice"
+        txtPrice.Size = New Size(125, 27)
+        txtPrice.TabIndex = 7
+        ' 
+        ' ddlBrand
+        ' 
+        ddlBrand.FormattingEnabled = True
+        ddlBrand.Location = New Point(178, 194)
+        ddlBrand.Name = "ddlBrand"
+        ddlBrand.Size = New Size(151, 28)
+        ddlBrand.TabIndex = 8
+        ' 
+        ' ddlCategory
+        ' 
+        ddlCategory.FormattingEnabled = True
+        ddlCategory.Location = New Point(181, 240)
+        ddlCategory.Name = "ddlCategory"
+        ddlCategory.Size = New Size(151, 28)
+        ddlCategory.TabIndex = 9
+        ' 
+        ' productsGV
+        ' 
+        productsGV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        productsGV.Location = New Point(373, 69)
+        productsGV.Name = "productsGV"
+        productsGV.RowHeadersWidth = 51
+        productsGV.Size = New Size(566, 319)
+        productsGV.TabIndex = 10
+        ' 
+        ' btnSave
+        ' 
+        btnSave.Location = New Point(64, 299)
+        btnSave.Name = "btnSave"
+        btnSave.Size = New Size(94, 29)
+        btnSave.TabIndex = 11
+        btnSave.Text = "Save"
+        btnSave.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(196, 300)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(94, 29)
+        btnClear.TabIndex = 12
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btnUpdate
+        ' 
+        btnUpdate.Location = New Point(65, 342)
+        btnUpdate.Name = "btnUpdate"
+        btnUpdate.Size = New Size(94, 29)
+        btnUpdate.TabIndex = 13
+        btnUpdate.Text = "Update"
+        btnUpdate.UseVisualStyleBackColor = True
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.Location = New Point(197, 342)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(94, 29)
+        btnDelete.TabIndex = 14
+        btnDelete.Text = "Delete"
+        btnDelete.UseVisualStyleBackColor = True
+        ' 
+        ' btnShow
+        ' 
+        btnShow.Location = New Point(67, 392)
+        btnShow.Name = "btnShow"
+        btnShow.Size = New Size(224, 29)
+        btnShow.TabIndex = 15
+        btnShow.Text = "Show All"
+        btnShow.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(971, 450)
+        Controls.Add(btnShow)
+        Controls.Add(btnDelete)
+        Controls.Add(btnUpdate)
+        Controls.Add(btnClear)
+        Controls.Add(btnSave)
+        Controls.Add(productsGV)
+        Controls.Add(ddlCategory)
+        Controls.Add(ddlBrand)
+        Controls.Add(txtPrice)
+        Controls.Add(txtModel)
+        Controls.Add(txtProduct)
+        Controls.Add(lblCategory)
+        Controls.Add(lblBrand)
+        Controls.Add(lblUnitPrice)
+        Controls.Add(lblModel)
+        Controls.Add(lblProduct)
+        Name = "Form1"
+        Text = "Form1"
+        CType(productsGV, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblProduct As Label
+    Friend WithEvents lblModel As Label
+    Friend WithEvents lblUnitPrice As Label
+    Friend WithEvents lblBrand As Label
+    Friend WithEvents lblCategory As Label
+    Friend WithEvents txtProduct As TextBox
+    Friend WithEvents txtModel As TextBox
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents ddlBrand As ComboBox
+    Friend WithEvents ddlCategory As ComboBox
+    Friend WithEvents productsGV As DataGridView
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnShow As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,195 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Dim selectedRowId = 0
+    Dim brandId = 0
+    Dim categoryId = 0
+    Dim connectionString As String = "Data Source=ZENX\SQLEXPRESS04; Initial Catalog=BikeStores; Integrated Security = True"
+    Dim con As SqlConnection
+    Dim cmd As New SqlCommand
+    Dim sda As SqlDataAdapter
+    Dim sdaCat As SqlDataAdapter
+    Dim dt As DataTable
+    Dim dtBrand As DataTable
+    Dim dtCat As DataTable
+    Dim drc As DataGridViewRowCollection
+    Dim dr As DataGridViewRow
+    Dim dlgResult As DialogResult
+
+    Dim query = ""
+    ''' <summary>
+    ''' Fill grid view with Products Data
+    ''' </summary>
+    Private Sub FillGrid()
+        con = New SqlConnection(connectionString)
+        cmd = New SqlCommand("Select * from production.products ORDER BY product_id DESC", con)
+        con.Open()
+        sda = New SqlDataAdapter(cmd)
+        dt = New DataTable()
+        sda.Fill(dt)
+
+        productsGV.DataSource = dt
+        con.Close()
+
+    End Sub
+
+
+    '<summary>
+    ' To fill Brand And Category dropdowns
+    ' </summary>
+    Private Sub FillData()
+        cmd = New SqlCommand("Select * from production.brands", con)
+        con.Open()
+        sda = New SqlDataAdapter(cmd)
+        dtBrand = New DataTable()
+        sda.Fill(dtBrand)
+
+        ddlBrand.DataSource = dtBrand
+        ddlBrand.DisplayMember = "brand_name"
+        ddlBrand.ValueMember = "brand_id"
+
+        cmd = New SqlCommand("Select * from production.categories", con)
+        sdaCat = New SqlDataAdapter(cmd)
+        dtCat = New DataTable()
+        sdaCat.Fill(dtCat)
+
+        ddlCategory.DataSource = dtCat
+        ddlCategory.DisplayMember = "category_name"
+        ddlCategory.ValueMember = "category_id"
+
+        con.Close()
+    End Sub
+
+    Private Sub txtProduct_TextChanged(sender As Object, e As EventArgs) Handles txtProduct.TextChanged
+        ValidateForm()
+    End Sub
+
+    Private Sub txtPrice_TextChanged(sender As Object, e As EventArgs) Handles txtPrice.TextChanged
+        ValidateForm()
+    End Sub
+
+    Private Sub txtModel_TextChanged(sender As Object, e As EventArgs)
+        ValidateForm()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        con.Open()
+        cmd = New SqlCommand("sp_addUpdateProductData", con)
+        cmd.CommandType = CommandType.StoredProcedure
+        cmd.Parameters.AddWithValue("@productName", txtProduct.Text)
+        cmd.Parameters.AddWithValue("@brandId", ddlBrand.SelectedValue)
+        cmd.Parameters.AddWithValue("@categoryId", ddlCategory.SelectedValue)
+        cmd.Parameters.AddWithValue("@model", txtModel.Text)
+        cmd.Parameters.AddWithValue("@unitPrice", txtPrice.Text)
+        cmd.ExecuteNonQuery()
+        MessageBox.Show("Product added successfully.", "Record Added", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        con.Close()
+        Clear()
+        FillGrid()
+    End Sub
+
+    Private Sub Clear()
+        txtProduct.Clear()
+        txtModel.Clear()
+        txtPrice.Clear()
+        ddlBrand.SelectedIndex = -1
+        ddlCategory.SelectedIndex = -1
+        selectedRowId = brandId = categoryId = 0
+        ValidateForm()
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        If selectedRowId > 0 Then
+            con.Open()
+            cmd = New SqlCommand("sp_addUpdateProductData", con)
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Parameters.AddWithValue("@productId", selectedRowId)
+            cmd.Parameters.AddWithValue("@productName", txtProduct.Text)
+            cmd.Parameters.AddWithValue("@brandId", ddlBrand.SelectedValue)
+            cmd.Parameters.AddWithValue("@categoryId", ddlCategory.SelectedValue)
+            cmd.Parameters.AddWithValue("@model", txtModel.Text)
+            cmd.Parameters.AddWithValue("@unitPrice", txtPrice.Text)
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Product added successfully.", "Record Added", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            con.Close()
+            Clear()
+            FillGrid()
+        Else
+            MessageBox.Show("Please select a grid row to update.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        dlgResult = MessageBox.Show("Are you sure you want to delete this record ? ", "Delete record", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk)
+        If (dlgResult.ToString() Is "Yes") Then
+            query = "delete from production.products where product_id = " + selectedRowId
+
+            con.Open()
+            cmd = New SqlCommand(query, con)
+            cmd.ExecuteNonQuery()
+            con.Close()
+            FillGrid()
+            Clear()
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        Clear()
+    End Sub
+
+    Private Sub ValidateForm()
+        If (txtProduct.Text IsNot "" And
+        txtModel.Text IsNot "" And
+        txtPrice.Text IsNot "" And
+        ddlBrand.SelectedValue IsNot Nothing And
+        ddlCategory.SelectedValue IsNot Nothing) Then
+            If selectedRowId > 0 Then
+                btnUpdate.Enabled = True
+                btnDelete.Enabled = True
+            Else
+                btnSave.Enabled = True
+            End If
+        Else
+            btnSave.Enabled = False
+            btnUpdate.Enabled = False
+            btnDelete.Enabled = False
+        End If
+    End Sub
+
+    Private Sub productsGV_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles productsGV.CellContentClick
+        drc = productsGV.Rows
+        dr = drc(e.RowIndex)
+        selectedRowId = Convert.ToInt32(dr.Cells(0).Value)
+        brandId = Convert.ToInt32(dr.Cells(2).Value) - 1
+        categoryId = Convert.ToInt32(dr.Cells(3).Value) - 1
+
+        ddlBrand.SelectedIndex = brandId
+        ddlCategory.SelectedIndex = categoryId
+        txtProduct.Text = dr.Cells(1).Value.ToString()
+        txtModel.Text = dr.Cells(4).Value.ToString()
+        txtPrice.Text = dr.Cells(5).Value.ToString()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FillGrid()
+        FillData()
+    End Sub
+
+    Private Sub ddlBrand_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ddlBrand.SelectedIndexChanged
+        ValidateForm()
+    End Sub
+
+    Private Sub ddlCategory_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ddlCategory.SelectedIndexChanged
+        ValidateForm()
+    End Sub
+End Class
+
+
+
+
+
+'// Private Sub txtProduct_TextChanged(sender As Object, e As EventArgs)
+
+' End Sub
+
